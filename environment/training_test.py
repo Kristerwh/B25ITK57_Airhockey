5b991ec5b29ded5c92d48e38c73af88a4a9eef2f
Krister <krister.w.h@hotmail.no>
2025-04-01 13:35:13 +0200

    some useful comments and cleanup

--- environment/training_test.py
+++ environment/training_test.py
@@ -58,31 +58,31 @@ obs = env.reset()
 with (mujoco.viewer.launch_passive(model, data) as viewer):
     step = 0
     while viewer.is_running():
+        # Variables for position
         print("Initial obs:", obs)
         base_pos = np.array([data.qpos[5], data.qpos[6]])
-        notpixel_pos = float(data.xpos[puck_id][0]), float(data.xpos[puck_id][1])
-        print("notpixel_puck_pos:", notpixel_pos)
         puck_pos = float(data.xpos[puck_id][0] * 1000) + 974, float(data.xpos[puck_id][1] * 1000) + 519
         puck_pos_reverted = 2 * 974 - (float(data.xpos[puck_id][0] * 1000) + 974), 2 * 519 - (float(data.xpos[puck_id][1] * 1000) + 519)
         mallet_pos_script_ai = float(data.xpos[paddle_id][0] * 1000) + 974, float(data.xpos[paddle_id][1] * 1000) + 519
         mallet2_pos_script_ai = 2 * 974 - (float(data.xpos[paddle_id2][0] * 1000) + 974), 2 * 519 - (float(data.xpos[paddle_id2][1] * 1000) + 519)
 
+        # Action1 = RLAI, Action2 ScriptedAI
         action1 = agent.predict(obs)
         action2 = script.run(scripted_ai2, puck_pos_reverted, mallet2_pos_script_ai)
         action2 = np.array([-action2[0], -action2[1]])
         action = np.concatenate((action1, action2))
 
+        # Sending AI's actions to the controller
         control_action = controller.apply_action(action)
         data.ctrl[:2] = control_action[:2]
         data.ctrl[2:4] = control_action[2:4]
 
         next_obs, reward, absorbing, _ = env.step(action1)
+        mujoco.mj_step(model, data)
 
         if reward > 0:
             target_action = action1 + reward * 0.05
             agent.fit(np.array([obs]), np.array([target_action]), epochs=1)
-
-        mujoco.mj_step(model, data)
 
         if step % 10 == 0:
             mujoco.mjv_updateScene(model, data, mujoco.MjvOption(), None, camera, mujoco.mjtCatBit.mjCAT_ALL, scene)
